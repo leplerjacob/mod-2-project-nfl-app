69c617a670957f6766884d74ce2b52515d33c262
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-11 16:20:10 -0500

    created MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :coaches
+  resources :stadia
+  resources :matches
+  resources :teams
+  resources :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
