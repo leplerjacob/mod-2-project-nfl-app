565aa8f788dd6686955d16d122770bdfd076d15a
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-16 08:25:38 -0500

    added ALL links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   # Temp Homepage
-  root 'topics#index'
-
+  root "sessions#new"
+  
   # Coaches
   resources :coaches, only: [:index, :show]
   get '/coachpositions', to: 'coaches#position'
@@ -15,17 +15,18 @@ Rails.application.routes.draw do
   
   # Users
   resources :users
-
+  
   # Sessions
   get 'login', to: 'sessions#new', as: 'user_login'
   post 'login', to: 'sessions#create', as: 'login'
   
   # Teams
   resources :teams, only: [:index, :show]
-
+  
   # Players
   resources :players, only: [:index, :show]
   get '/playerpositions', to: 'players#position'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
